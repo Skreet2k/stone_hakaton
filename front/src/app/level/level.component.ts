aaa2fd6371dae5b94c789adff3a79320bb732501
Alexander Kharkovskiy <alexander.kharkovskiy@simbirsoft.com>
2024-09-07 18:43:45 +0400

    add level

---
+++ front/src/app/level/level.component.ts
@@ -0,0 +1,25 @@
+import { NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ClickCounterService } from '../click-counter.service';
+
+@Component({
+  selector: 'app-level',
+  standalone: true,
+  imports: [NgStyle],
+  templateUrl: './level.component.html',
+  styleUrl: './level.component.scss'
+})
+export class LevelComponent implements OnInit {
+  progress: number = 0
+  level: number = 0;
+
+  constructor(private clickService: ClickCounterService) { }
+
+  ngOnInit() {
+    this.clickService.getClickCount().subscribe(r => {
+      this.progress = r % 100;
+      this.level = Math.floor(r / 100);
+    });
+  }
+
+}
